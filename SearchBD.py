d0d1c1d3b7373aaf46b4e129eddd4479df8fc75a
PavelKoro <p.korol2003@gmail.com>
2024-11-30 08:15:36 +0300

    Готовый поиск

--- SearchBD.py
+++ SearchBD.py
@@ -1,13 +1,7 @@
-from DownloadDataBD import upload_database
 from melon_search import poisk
+from openai import OpenAI
 
-file_path = 'DataTXT/1.txt'
-name_db = 'file_data'
-size_vec = 1024
-collec = 'txt_2'
-
-upload_database(file_path, name_db, collec, size_vec)
-
-def process_query(query: str) -> str:
-    promt = poisk(query, name_db, collec)
+def process_query(user_id, query: str) -> str:
+    print(f"USER_ID: {user_id}")
+    promt = poisk(user_id, query, name_db = "file_data", collec = "txt_2", size_vec = 1024)
     return promt
